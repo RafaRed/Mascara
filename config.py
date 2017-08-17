174194756b063ffaab6a70463d536e5e894f007c
RafaRed <rafaelszcardoso@gmail.com>
2017-08-17 00:41:58 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+topDir = "/Mascara/DataFiles/TopWord"
+timer = 1;
+timerMax = 2;
+configDir = "/Mascara/query.py"
+lastQueryDir = "/Mascara/DataFiles/LastQuery"
+blacklistDir = "/Mascara/DataFiles/BlackList"
+user='teste@gmail.com'
+passs='teste'
